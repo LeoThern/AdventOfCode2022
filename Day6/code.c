706e9409608585a4dc5277ec83326ecc9ed9d9ca
Leo Thern <47295238+LeoThern@users.noreply.github.com>
2022-12-06 16:16:55 +0100

    day6 c both tasks

--- Day6/code.c
+++ Day6/code.c
@@ -1,39 +1,58 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void push_left(char* arr, int len){
-    for (int i = 0; i < len; i++){
+void push_back(char* arr, int len, char value){
+    for (int i = 0; i < len - 1; i++){
         arr[i] = arr[i+1];
     }
+    arr[len - 1] = value;
 }
 
-void print_arr(char* arr, int len){
-    printf("[");
-    for (int i = 0; i <= len; i++)
-        printf("%c,", arr[i]);
-    printf("]");
+bool are_all_unique(char* arr, int len){
+    for(int i = 0; i < len - 1; i++){
+        for(int j = i + 1; j < len; j++){
+            if(arr[i] == arr[j])
+                return false;
+        }
+    }
+    return true;
 }
 
-int index_x_unique(FILE* file, int x){
-    char current_chars[x];
-    for (int i = 0; i < x; i++)
+int index_x_unique(FILE* file, int length){
+    char current_chars[length];
+    for (int i = 0; i < length; i++)
         current_chars[i] = fgetc(file);
-    
-    print_arr(current_chars, x);
-    push_left(current_chars, x);
-    print_arr(current_chars, x);
 
+    int i = length;
     while(1){
+        if (are_all_unique(current_chars, length))
+            return i;
+
         char c = fgetc(file);
         if (c == EOF)
             break;
-        //printf("%c",c);
+        push_back(current_chars, length, c);
+        i++;
     }
-    return 0;
+    return -1;
+}
+
+void exec_task1(){
+    FILE* file_pointer = fopen("input.txt", "r");
+    int result = index_x_unique(file_pointer, 4);
+    fclose(file_pointer);
+    printf("Task1 (4 unique): %i\n", result);
+}
+
+void exec_task2(){
+    FILE* file_pointer = fopen("input.txt", "r");
+    int result = index_x_unique(file_pointer, 14);
+    fclose(file_pointer);
+    printf("Task1 (14 unique): %i\n", result);
 }
 
 int main(void){
-    FILE* file_pointer = fopen("input.txt", "r");
-    int result = index_x_unique(file_pointer, 5);
-    fclose(file_pointer);
+    exec_task1();
+    exec_task2();
     return 0;
 }
